07def0218ded4988ba05949734bbca445c466fd5
pierre0210 <gamelauncher0210@gmail.com>
2023-04-30 21:44:14 +0800

    refactor: change model url

--- main_test.py
+++ main_test.py
@@ -21,8 +21,9 @@ center_x = round(cam_width / 2)
 center_y = round(cam_height / 2)
 
 def check_model():
-    model_url = f"https://github.com/davisking/dlib-models/raw/master/shape_predictor_68_face_landmarks_GTX.dat.bz2"
-    model_name = "shape_predictor_68_face_landmarks_GTX.dat"
+    # model_url = f"https://github.com/davisking/dlib-models/raw/master/shape_predictor_68_face_landmarks_GTX.dat.bz2"
+    model_url = "http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2"
+    model_name = "shape_predictor_68_face_landmarks.dat"
     if not os.path.exists(model_name):
         urlretrieve(model_url, model_name + '.bz2')
         with open(model_name, 'wb') as decompressed_file, open(model_name + '.bz2', 'rb') as commpressed_file:
@@ -56,9 +57,9 @@ def main():
             shape = predictor(resized, rect)
             shape = face_utils.shape_to_np(shape)
             
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
+            cv2.rectangle(resized, (x, y), (x + w, y + h), (0, 0, 255), 1)
             for (x, y) in shape:
-                cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)
+                cv2.circle(resized, (x, y), 2, (0, 255, 0), -1)
             mouth = shape[49:61]
             x_min, y_min = mouth.min(axis=0)
             x_max, y_max = mouth.max(axis=0)
@@ -66,10 +67,10 @@ def main():
             x_mid, y_mid = round((x_max + x_min) / 2), round((y_max + y_min) / 2)
             h_angle , v_angle = ((x_mid - center_x) / center_x) * (h_FOV / 2), ((y_mid - center_y) / center_y) * (v_FOV / 2)
             print("{} {}".format(h_angle, v_angle))
-            cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (255, 0, 0), 1)
-            cv2.circle(frame, (x_mid, y_mid), 4, (255, 0, 0), -1)
+            cv2.rectangle(resized, (x_min, y_min), (x_max, y_max), (255, 0, 0), 1)
+            cv2.circle(resized, (x_mid, y_mid), 4, (255, 0, 0), -1)
         
-        cv2.imshow("Frame", frame)
+        cv2.imshow("Frame", resized)
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             break
